4cdc25d99dcd37b658e7daaa0f95fd3d3320f919
Miklos Szeredi <mszeredi@redhat.com>
2020-12-14 15:26:14 +0100

    ovl: simplify file splice
    commit 82a763e61e2b601309d696d4fa514c77d64ee1be upstream.
    
    generic_file_splice_read() and iter_file_splice_write() will call back into
    f_op->iter_read() and f_op->iter_write() respectively.  These already do
    the real file lookup and cred override.  So the code in ovl_splice_read()
    and ovl_splice_write() is redundant.
    
    In addition the ovl_file_accessed() call in ovl_splice_write() is
    incorrect, though probably harmless.
    
    Fix by calling generic_file_splice_read() and iter_file_splice_write()
    directly.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    [reported to resolve issues with 1a980b8cbf00 ("ovl: add splice file read write helper")]
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/overlayfs/file.c
+++ fs/overlayfs/file.c
@@ -634,7 +634,8 @@ const struct file_operations ovl_file_operations = {
 	.fadvise	= ovl_fadvise,
 	.unlocked_ioctl	= ovl_ioctl,
 	.compat_ioctl	= ovl_compat_ioctl,
-
+	.splice_read    = generic_file_splice_read,
+	.splice_write   = iter_file_splice_write,
 	.copy_file_range	= ovl_copy_file_range,
 	.remap_file_range	= ovl_remap_file_range,
 };
